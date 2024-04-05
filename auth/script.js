f01f5247dc1085ac7ccdc274413dd51c8cf6591f
Dorize <31163880+zdorize@users.noreply.github.com>
2024-04-04 21:42:26 -0400

    Update script.js

--- auth/script.js
+++ auth/script.js
@@ -2,7 +2,7 @@ const msalConfig = {
     auth: {
         clientId: '25270647-3321-4e4d-9f26-3a6bc9ae725f',
         authority: 'https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a',
-        redirectUri: 'https://pika.rip/auth/callback',
+        redirectUri: 'https://pika.rip/auth/',
         knownAuthorities: ['https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a']
     }
 };
